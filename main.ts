8cff4b98fa0fd87d1f396e2f64fe26fbbfebefda
maytees <mateespublicprofile@gmail.com>
2023-05-02 16:50:55 -0400

    put

--- main.ts
+++ main.ts
@@ -40,6 +40,47 @@ router.get("/targets", async (ctx) => {
   ctx.response.body = JSON.parse(await Deno.readTextFile("./people.json"));
 });
 
+router.post("/targets", async (ctx) => {
+  type BodyStuff = {
+    name: string;
+    blocked: boolean;
+  };
+
+  const body: BodyStuff = await ctx.request.body().value;
+  const newTarget: Person = {
+    name: body.name,
+    blocked: body.blocked,
+  };
+
+  const stringified: string = JSON.stringify(newTarget);
+  await Deno.writeTextFile("./people.json", stringified);
+  ctx.response.body = stringified;
+});
+
+// toggle if no onoff specified
+router.put("/targets/:name", async (ctx) => {
+  const params = await ctx.params;
+
+  if (!params.name) {
+    ctx.response.status = 401;
+    ctx.response.body = "Please provide a name";
+  }
+
+  const people: People = JSON.parse(await Deno.readTextFile("./people.json"));
+  const targetIndex: number = people.findIndex((p) => p.name === params.name);
+
+  const target: Person = people[targetIndex];
+
+  if (!params.onoff) {
+    target.blocked = !target.blocked;
+    ctx.response.body = target;
+    return;
+  }
+
+  target.blocked = Boolean(params.onoff);
+  ctx.response.body = target;
+});
+
 app.use(router.allowedMethods());
 app.use(router.routes());
 
