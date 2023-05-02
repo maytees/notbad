69b8af548b1b0d0a0f1e53c5a82b0c3920e203a4
maytees <mateespublicprofile@gmail.com>
2023-05-02 08:44:36 -0400

    init

---
+++ main.ts
@@ -0,0 +1,31 @@
+import { Application } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+
+const app = new Application();
+
+type People = Person[];
+
+type Person = {
+  name: string;
+  blocked: boolean;
+};
+
+app.use((ctx) => {
+  const people: People = JSON.parse(Deno.readTextFileSync("people.json"));
+  const name: string | null = ctx.request.url.searchParams.get("name");
+  if (!name) {
+    ctx.response.body = "provide name param";
+    return;
+  }
+
+  for (const person of people) {
+    if (name !== person.name) continue;
+
+    if (person.blocked) {
+      ctx.response.body = "yes";
+      return;
+    }
+  }
+  ctx.response.body = "no";
+});
+
+await app.listen({ port: 8000 });
