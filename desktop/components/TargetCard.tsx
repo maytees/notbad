331e4515cbdfab8cff9a26d049d502fecf1eb7b5
maytees <mateespublicprofile@gmail.com>
2023-05-02 21:47:43 -0400

    create

--- desktop/components/TargetCard.tsx
+++ desktop/components/TargetCard.tsx
@@ -19,7 +19,8 @@ const TargetCard: React.FC<TargetCardProps> = (
   };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-y-2 w-60 h-28 bg-gray-900 rounded-lg">
+    <div className="flex flex-col justify-center items-center gap-y-2
+     w-60 h-28 bg-none outline outline-2 outline-error ring-1 shadow-sm rounded-lg">
       <h1 className="text-left text-2xl font-bold">{name}</h1>
       <input
         type="checkbox"
--- desktop/pages/index.tsx
+++ desktop/pages/index.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import TargetCard from "@/components/TargetCard";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 type Target = {
   name: string;
@@ -12,6 +15,8 @@ type Target = {
 
 export default function Home() {
   const [targets, setTargets] = useState<Target[]>([]);
+  const [nameField, setNameField] = useState<string>("");
+  const [blockedField, setBlockedField] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchedTargets = async () => {
@@ -27,10 +32,94 @@ export default function Home() {
     fetchedTargets();
   }, []);
 
+  const createNewTarget = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (nameField === "") {
+      console.log("Name field is empty");
+      return;
+    }
+
+    const data = await (
+      await fetch(
+        "https://vizard.matees.net/targets",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: nameField,
+            blocked: blockedField,
+          }),
+        },
+      )
+    ).json();
+
+    setTargets([data]);
+    setNameField("");
+    setBlockedField(true);
+  };
+
   return (
-    <div className="text-error w-screen h-screen flex flex-col items-center justify-start pt-10">
+    <div
+      className={`${inter.className} text-error w-screen h-screen flex flex-col gap-y-10 items-center justify-start pt-10`}
+    >
       <h1 className="text-4xl text-error font-extrabold">Not bad</h1>
-      <div className="flex flex-row justify-between items-between w-screen gap-y-10 flex-wrap p-10 mt-5">
+
+      <label htmlFor="my-modal-3" className="btn btn-error text-white">
+        Create new
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative outline outline-1 outline-red-200 shadow-xl">
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-error btn-circle absolute right-2 top-2 text-white"
+          >
+            ✕
+          </label>
+          <h1 className="text-xl font-bold">
+            Create new target
+          </h1>
+
+          <form onSubmit={createNewTarget}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-red-400">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                value={nameField}
+                onChange={(e) => setNameField(e.target.value)}
+                className="input input-bordered input-error"
+              />
+            </div>
+            {/* Create form control with label for a toggle which toggles blocked or not (daisy ui) */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-red-400">Blocked</span>
+              </label>
+              <input
+                type="checkbox"
+                checked={blockedField}
+                defaultChecked
+                onClick={() => setBlockedField(!blockedField)}
+                className="toggle toggle-error"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                value="Create"
+                className="btn btn-error text-white"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between items-between w-screen gap-y-10 flex-wrap px-10">
         {targets.map((t) => (
           <TargetCard key={t.name} name={t.name} checked={t.blocked} />
         ))}
