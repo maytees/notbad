4078d8cfc3b0a686105b0e996f6e16bebb14230f
maytees <mateespublicprofile@gmail.com>
2023-05-02 20:15:52 -0400

    get all targets

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
 const app = new Application();
 
@@ -9,7 +9,10 @@ type Person = {
   blocked: boolean;
 };
 
-app.use(async (ctx) => {
+const router = new Router();
+
+// @ts-ignore: idk the type
+router.get("/", async (ctx) => {
   const people: People = JSON.parse(await Deno.readTextFile("./people.json"));
   const name: string | null = ctx.request.url.searchParams.get("name");
   if (!name) {
@@ -30,5 +33,14 @@ app.use(async (ctx) => {
   }
   ctx.response.body = "nobody found..";
 });
+
+// @ts-ignore: idk the type
+router.get("/targets", async (ctx) => {
+  ctx.response.headers.set("Content-Type", "application/json");
+  ctx.response.body = JSON.parse(await Deno.readTextFile("./people.json"));
+});
+
+app.use(router.allowedMethods());
+app.use(router.routes());
 
 await app.listen({ port: 8000 });
